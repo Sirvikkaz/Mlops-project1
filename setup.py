6adc42edad09df045a20bb71dce02bcbc63b230d
Sirvikkaz <olanrewajuvictor2018@gmail.com>
2026-06-26 21:52:59 +0100

    Project structure ready, venv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sir Vikkaz",
+    author_email="olanrewajuvictor2018@gmail.com",
+    packages=find_packages()
+)
